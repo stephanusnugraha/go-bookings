467c6d901de99a017ffeba43fcea8eebf3982917
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-03-02 18:14:09 +0700

    add form validation when field is blank

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -5,6 +5,11 @@ import (
 	"net/url"
 )
 
+// Valid returns true if there are no errors, otherwise false
+func (f Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
 // Form creates a custom form struct, embeds a url.Values object
 type Form struct {
 	url.Values
@@ -23,6 +28,7 @@ func New(data url.Values) *Form {
 func (f Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
+		f.Errors.Add(field, "This field cannot be blank")
 		return false
 	}
 	return true
